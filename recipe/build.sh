9f068d967dc82733da38facccae669f02f8b52e9
Huidae Cho <grass4u@gmail.com>
2026-01-12 14:59:13 -0700

    Test building native and cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 set -e
 
-case "$target_platform" in
-osx-*)
-	with_others="
-		--with-opengl=osx
-		--with-x=no
-	"
-	;;
-esac
+echo "===== conda-build env ====="
+echo "CONDA_BUILD_CROSS_COMPILATION=${CONDA_BUILD_CROSS_COMPILATION}"
+echo "target_platform=${target_platform}"
+echo "BUILD=${BUILD}"
+echo "HOST=${HOST}"
+echo "PREFIX=${PREFIX}"
+echo "BUILD_PREFIX=${BUILD_PREFIX}"
+echo "CC=${CC}"
+echo "CXX=${CXX}"
+echo "CC_FOR_BUILD=${CC_FOR_BUILD}"
+echo "CXX_FOR_BUILD=${CXX_FOR_BUILD}"
+echo "CFLAGS_FOR_BUILD=${CFLAGS_FOR_BUILD}"
+echo "CXXFLAGS_FOR_BUILD=${CXXFLAGS_FOR_BUILD}"
+echo "LDFLAGS_FOR_BUILD=${LDFLAGS_FOR_BUILD}"
+echo "AR_FOR_BUILD=${AR_FOR_BUILD}"
+echo "RANLIB_FOR_BUILD=${RANLIB_FOR_BUILD}"
 
-CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY" \
-./configure \
-	--prefix=$PREFIX \
+export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
+
+configure_opts="\
 	--with-blas \
 	--with-bzlib \
 	--with-lapack \
@@ -21,9 +29,37 @@ CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY" \
 	--with-postgres \
 	--with-pthread \
 	--with-readline \
-	$with_others ||
-	(echo "===== config.log =====" && cat config.log && exit 1)
+"
 
+case "$target_platform" in
+osx-*)
+	configure_opts="$configure_opts \
+		--with-opengl=osx \
+		--with-x=no \
+"
+	;;
+esac
+
+if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
+	mkdir -p build-tools
+	(cd build-tools
+
+	export CC="${CC_FOR_BUILD}"
+	export CXX="${CXX_FOR_BUILD}"
+	export AR="${AR_FOR_BUILD}"
+	export RANLIB="${RANLIB_FOR_BUILD}"
+
+	../configure --host="$BUILD" $configure_opts ||
+	(echo "===== build-tools config.log =====" && cat config.log && exit 1)
+
+	for tool in general/g.parser; do
+		make -j$CPU_COUNT -C $tool
+	done)
+	ls -al "$(pwd)/dist.$BUILD/bin"
+fi
+
+./configure --prefix=$PREFIX $configure_opts ||
+	(echo "===== config.log =====" && cat config.log && exit 1)
 sed -Ei 's/^(ICONVLIB *= *$)/\1-liconv/' include/Make/Platform.make
 
 make -j$CPU_COUNT
