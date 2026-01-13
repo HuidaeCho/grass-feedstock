104f40b0c05519c4c2394d0325e78f04936eb53d
Huidae Cho <grass4u@gmail.com>
2026-01-13 02:05:35 -0700

    Quote $build_dist

--- recipe/build.sh
+++ recipe/build.sh
@@ -74,6 +74,6 @@ sed -Ei 's/^(ICONVLIB *= *$)/\1-liconv/' include/Make/Platform.make
 make -j$CPU_COUNT
 
 [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] &&
-	GISRC=junk GISBASE="dist.$HOST" $build_dist/bin/g.mkfontcap
+	GISRC=junk GISBASE="dist.$HOST" "$build_dist/bin/g.mkfontcap"
 
 make install
