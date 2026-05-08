94ce07052f1ed90d6bca1434b7d59455d614eaa1
Huidae Cho <grass4u@gmail.com>
2026-05-08 10:46:36 -0600

    Add missing_dso_whitelist to avoid RPATH/grass*.dll not found errors on Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,15 @@
 set -e
 
 if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
+	(
 	export PYTHON="${BUILD_PREFIX}/bin/python"
+	export CC="$CC_FOR_BUILD"
+	export CXX="$CXX_FOR_BUILD"
+	export AR="$AR_FOR_BUILD"
+	export RANLIB="$RANLIB_FOR_BUILD"
+	export CPPFLAGS="-I$BUILD_PREFIX/include $CPPFLAGS"
+	export LDFLAGS="-L$BUILD_PREFIX/lib $LDFLAGS"
+	)
 fi
 
 cmake -B build $CMAKE_ARGS \
