37329f448cb9cd29a6736fe977fe6e9eb1f32a89
Huidae Cho <grass4u@gmail.com>
2026-05-08 10:06:56 -0600

    Use build Python for cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
+	export PYTHON="${BUILD_PREFIX}/bin/python"
+fi
+
 cmake -B build $CMAKE_ARGS \
 	-DPython3_EXECUTABLE=$PYTHON \
 	-DCMAKE_CXX_FLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
